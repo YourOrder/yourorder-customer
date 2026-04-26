a7b0db362c6b4fb224dddcb01143cc3fb1108176
Терещенко Степан <tereschenkostep@gmail.com>
2026-04-26 12:07:57 +0500

    first commit

---
+++ src/main/java/org/example/yourordercustomer/YourorderCustomerApplication.java
@@ -0,0 +1,13 @@
+package org.example.yourordercustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourorderCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourorderCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/yourordercustomer/YourorderCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.yourordercustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourorderCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
